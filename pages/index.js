e07765d1e78bebb50ef30979f73c90c69e6e6f8e
unknown <mr.pokorski@gmail.com>
2022-05-08 14:29:20 +0200

    add skip link to content for screen readers

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,15 @@ import Header from "../components/Header";
 const Home = () => {
   return (
     <>
+      <a
+        href="#tresc"
+        class="fixed py-4 px-8 m-4 bg-gray-500 text-white skip-link"
+      >
+        Przejdź do treści
+      </a>
       <Header isHome logoAsTitle />
+      <main id="tresc"></main>
+      <Footer isHome />
     </>
   );
 };
