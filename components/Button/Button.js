7f5ab352e4ca8bc8d0e613dd64e908bc276cd4d8
unknown <mr.pokorski@gmail.com>
2022-05-09 00:00:10 +0200

    modify Button component - delegate link computing to custom Link component

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,27 +1,15 @@
-import Link from "next/link";
+import Link from "../Link";
 
 const Button = ({ label, href, customClasses }) => {
-  const isInternalLink = href && (href.startsWith("/") || href.startsWith("#"));
-
-  return isInternalLink ? (
-    <Link href={href}>
-      <a
-        className={`inline-block px-4 md:px-6 py-2 md:py-3 transition-all ${
-          customClasses ? customClasses : "bg-green hover:bg-greenHover "
-        }`}
-      >
-        {label}
-      </a>
-    </Link>
-  ) : (
-    <a
+  return (
+    <Link
       href={href}
       className={`inline-block px-4 md:px-6 py-2 md:py-3 transition-all ${
         customClasses ? customClasses : "bg-green hover:bg-greenHover "
       }`}
     >
       {label}
-    </a>
+    </Link>
   );
 };
 
