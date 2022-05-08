fb6c236d20578b6129049ffb42786c764bbcdab9
unknown <mr.pokorski@gmail.com>
2022-05-09 01:43:15 +0200

    change Hero component - description have to be a html

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -15,7 +15,7 @@ const Hero = ({ title, description, buttons, image, bgColor, isHome }) => {
               {title}
             </h2>
           )}
-          {description && <p>{description}</p>}
+          {description && description}
           {buttons && (
             <div className="flex flex-row flex-wrap gap-2 justify-center md:justify-start">
               {buttons.map((item, i) => (
