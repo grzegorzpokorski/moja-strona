4bbe37140919124736ebedd7c683d19a01ec3536
unknown <mr.pokorski@gmail.com>
2022-06-05 14:57:19 +0200

    change a date format in datetime attribute

--- components/Post/parts/Header.js
+++ components/Post/parts/Header.js
@@ -7,7 +7,7 @@ const Header = (props) => {
       <div className="container px-3 mx-auto w-full md:w-8/12">
         <span className="flex flex-row items-center mb-4 gap-3">
           <Link
-            href={`/blog/karegoria/${props.category}`}
+            href={`/blog/kategoria/${props.category}`}
             className="text-green hover:underline"
           >
             {props.category}
@@ -16,7 +16,7 @@ const Header = (props) => {
             className="inline-block w-12 h-0.5 bg-customGray text-separator"
             aria-hidden="true"
           ></span>
-          <time dateTime="2022.05.02">{props.date}</time>
+          <time dateTime={props.date.replaceAll(".", "-")}>{props.date}</time>
         </span>
         <h1 className="font-bold text-3xl md:text-4xl">{props.title}</h1>
         <figure className="w-full h-96 max-h-96 overflow-hidden relative block mt-12">
@@ -27,13 +27,6 @@ const Header = (props) => {
             priority
           />
         </figure>
-        {/* <figure className="w-full h-96 max-h-96 overflow-hidden relative block mt-12">
-      <source media="(min-width: 576px)" srcset="./dist/img/desk-768w.webp">
-      <source media="(min-width: 768px)" srcset="./dist/img/desk-992w.webp">
-      <source media="(min-width: 992px)" srcset="./dist/img/desk-1200w.webp">
-      <source media="(min-width: 1200px)" srcset="./dist/img/desk-1920w.webp">
-      <img width="558" height="500" src="./dist/img/desk-576w.webp" alt="człowiek pracuje przy biurku" loading="lazy" className="object-cover object-center w-full h-full">
-    </figure> */}
       </div>
     </header>
   );
