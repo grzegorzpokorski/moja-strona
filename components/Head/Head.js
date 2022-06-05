23199562a195308044e334b112dd9ba9a78ced56
unknown <mr.pokorski@gmail.com>
2022-06-05 23:27:53 +0200

    add favicons

--- components/Head/Head.js
+++ components/Head/Head.js
@@ -44,6 +44,24 @@ const Head = (props) => {
       )}
 
       <link rel="index" title="Strona główna" href={siteUrl} />
+
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
     </HeadTag>
   );
 };
