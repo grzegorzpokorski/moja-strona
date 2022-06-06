d4b6a69ae597bab8d48b04073188ebc4c233580f
unknown <mr.pokorski@gmail.com>
2022-06-06 21:12:34 +0200

    add Banner, and related PostsExcerpt to /blog/[slug].js

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -2,14 +2,20 @@ import MainHeader from "../../components/MainHeader";
 import Main from "../../components/Main";
 import Post from "../../components/Post";
 import Head from "../../components/Head";
+import Banner from "../../components/Banner";
 
-import { getPostBySlug, getPostsPaths } from "../../provider/posts";
+import {
+  getPostBySlug,
+  getPostsPaths,
+  getPublishedPostsOrderByDate,
+} from "../../provider/posts";
 import { serialize } from "next-mdx-remote/serialize";
 
 import siteName from "./../../data/seo/siteName";
 import addressSeparator from "../../data/seo/addressSeparator";
+import PostsExcerpt from "../../components/PostsExcerpt";
 
-const BlogPost = ({ post }) => {
+const BlogPost = ({ post, relatedPosts }) => {
   return (
     <>
       <Head
@@ -22,6 +28,24 @@ const BlogPost = ({ post }) => {
       <MainHeader></MainHeader>
       <Main withMarginOnTop>
         <Post post={post} />
+        {relatedPosts.length && (
+          <PostsExcerpt
+            subtitle="Posty o podobnej tematyce"
+            title="Mogą Cię zainteresować:"
+            posts={relatedPosts}
+          />
+        )}
+        <Banner
+          title="Zbuduj swoją wysokiej klasy stronę internetową ze mną!"
+          description="Chętnie pomogę zrealizować Twój projekt i porzegonić konkurencję."
+          buttons={[
+            {
+              label: "Skontaktuj się ze mną",
+              href: "/kontakt",
+              customClasses: "bg-white hover:bg-whiteGreen text-gray-800",
+            },
+          ]}
+        />
       </Main>
     </>
   );
@@ -31,10 +55,16 @@ export const getStaticProps = async ({ params }) => {
   const { slug } = params;
   const { frontmatter, source } = getPostBySlug(slug);
   const mdxSource = await serialize(source);
+  const relatedPosts = getPublishedPostsOrderByDate().filter(
+    (post) =>
+      post.frontmatter.category === frontmatter.category &&
+      post.frontmatter.slug != slug
+  );
 
   return {
     props: {
       post: { frontmatter, source: mdxSource },
+      relatedPosts: relatedPosts,
     },
   };
 };
