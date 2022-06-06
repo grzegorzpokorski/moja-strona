17ba0ca7a0e2ac4adb6a2379662b9d8fc29222ae
unknown <mr.pokorski@gmail.com>
2022-06-06 13:57:07 +0200

    refactor MenuItem component

--- components/MainMenu/parts/MenuItem.js
+++ components/MainMenu/parts/MenuItem.js
@@ -3,22 +3,17 @@ import Link from "../../Link";
 const MenuItem = (item) => {
   return (
     <li>
-      {item.button ? (
-        <Link
-          href={item.href}
-          className={`inline-block bg-green hover:bg-greenHover text-white px-6 py-3
-                transition-all ${item.childrens && "dropdown-toggle"}`}
-        >
-          {item.label}
-        </Link>
-      ) : (
-        <Link
-          href={item.href}
-          className="text-gray-100 lg:text-zinc-600 hover:text-gray-300 lg:hover:text-green transition-all"
-        >
-          {item.label}
-        </Link>
-      )}
+      <Link
+        href={item.href}
+        className={
+          item.button
+            ? `inline-block bg-green hover:bg-greenHover text-white px-6 py-3
+                transition-all ${item.childrens && "dropdown-toggle"}`
+            : `text-gray-100 lg:text-zinc-600 hover:text-gray-300 lg:hover:text-green transition-all`
+        }
+      >
+        {item.label}
+      </Link>
     </li>
   );
 };
