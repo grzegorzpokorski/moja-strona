5dcdcfa6966321e6bc28767cc4bb5891a60c0bdf
unknown <mr.pokorski@gmail.com>
2022-06-07 02:27:08 +0200

    add date to PostItem in PostList

--- components/PostsList/parts/PostItem.js
+++ components/PostsList/parts/PostItem.js
@@ -1,6 +1,15 @@
 import Link from "../../Link";
 import Image from "next/image";
 
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+dayjs.extend(customParseFormat);
+
+import locale_pl from "dayjs/locale/pl";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarDays, faTag } from "@fortawesome/free-solid-svg-icons";
+
 const PostItem = (post) => {
   return (
     <li className="bg-white drop-shadow-xl border-2 border-greenLight">
@@ -16,15 +25,31 @@ const PostItem = (post) => {
           </figure>
         </Link>
         <header className="p-8 flex flex-col gap-3">
-          <div className="flex flex-row gap-3">
+          <div className="flex flex-row gap-4">
             <Link
               href={`/blog/kategoria/${post.category}`}
-              className="text-green hover:underline"
+              className="hover:underline"
             >
+              <FontAwesomeIcon
+                icon={faTag}
+                className="mr-2 text-sm text-green"
+              />
               {post.category}
             </Link>
+            <time
+              dateTime={dayjs(post.date, "DD.MM.YYYY").format("YYYY-MM-DD")}
+            >
+              <FontAwesomeIcon
+                icon={faCalendarDays}
+                className="mr-2 text-sm text-green"
+              />
+              {dayjs(post.date).locale("pl").format("DD MMMM YYYY")}
+            </time>
           </div>
-          <Link href={`/blog/${post.slug}`} className="hover:underline">
+          <Link
+            href={`/blog/${post.slug}`}
+            className="hover:text-green transition"
+          >
             <h3 className="font-bold text-lg">{post.title}</h3>
           </Link>
           <p>{post.excerpt.split(" ").splice(0, 20).join(" ")} [...]</p>
