e62cd39e5a8e024023ab88865890650a1dda19ec
unknown <mr.pokorski@gmail.com>
2022-06-07 14:53:41 +0200

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,12 @@
+const productionHost =
+  process.env.NEXT_PUBLIC_HOST || "moja-strona-one.vercel.app";
+const devHost = "localhost:3000";
+const host = process.env.NODE_ENV === "production" ? productionHost : devHost;
+
+const protocol = process.env.NODE_ENV === "production" ? "https" : "http";
+
+module.exports = {
+  siteUrl: `${protocol}://${host}`,
+  generateRobotsTxt: true,
+  exclude: ["/admin"],
+};
