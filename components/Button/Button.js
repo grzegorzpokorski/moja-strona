800993bb6be56617cc2fa7a3f34c27d4aa119245
unknown <mr.pokorski@gmail.com>
2022-06-09 00:37:31 +0200

    add additional params to Button component

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,14 +1,17 @@
 import Link from "../Link";
 
-const Button = ({ label, href, customClasses }) => {
+const Button = ({ label, href, customClasses, small, gray }) => {
   return (
     <Link
       href={href}
-      className={`inline-block px-4 md:px-6 py-2 md:py-3 transition-all ${
+      className={`inline-block transition-all 
+      ${
         customClasses
           ? customClasses
           : "bg-green hover:bg-greenHover text-white"
-      }`}
+      } 
+      ${small ? "px-2 py-1 text-xs" : "px-4 md:px-6 py-2 md:py-3"} 
+      ${gray ? "bg-zinc-200 text-zinc-500 hover:text-white" : ""}`}
     >
       {label}
     </Link>
