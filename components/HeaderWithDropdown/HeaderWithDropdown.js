f6ca566e11b6a69392c558e105f717e70bf99a41
unknown <mr.pokorski@gmail.com>
2022-06-26 23:35:18 +0200

    Change handling functionality of closing Dropdown on click away
    Mainly changes which reflect changes in the project - instead of using inline useEffect, used ClickAwayListener component.

--- components/HeaderWithDropdown/HeaderWithDropdown.js
+++ components/HeaderWithDropdown/HeaderWithDropdown.js
@@ -1,8 +1,6 @@
 import Dropdown from "./parts/Dropdown";
 import Heading from "./parts/Heading";
 
-import { useState, useEffect } from "react";
-
 const HeaderWithSelectCategory = ({
   subtitle,
   title,
@@ -11,39 +9,6 @@ const HeaderWithSelectCategory = ({
   categories,
   initialDropdownValue,
 }) => {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-
-  useEffect(() => {
-    const handleEscapeKey = ({ key }) => {
-      key && key === "Escape" && setMobileMenuIsOpen(false);
-    };
-
-    const handleClickOffTheDropdown = ({ target }) => {
-      const dropdownMenu = document.querySelector("[data-name=dropdownMenu]");
-
-      do {
-        if (target === dropdownMenu) return;
-        target = target.parentNode;
-      } while (target != null);
-
-      setIsDropdownOpen(false);
-    };
-
-    if (isDropdownOpen) {
-      window.addEventListener("keydown", handleEscapeKey);
-      window.addEventListener("click", handleClickOffTheDropdown);
-    }
-
-    return () => {
-      window.removeEventListener("keydown", handleEscapeKey);
-      window.removeEventListener("click", handleClickOffTheDropdown);
-    };
-  }, [isDropdownOpen]);
-
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
-  };
-
   return (
     <header className={`flex flex-col lg:flex-row gap-6 lg:gap-20`}>
       <Heading
@@ -54,8 +19,6 @@ const HeaderWithSelectCategory = ({
       />
       <Dropdown
         initialDropdownValue={initialDropdownValue}
-        toggleDropdown={toggleDropdown}
-        isOpen={isDropdownOpen}
         categories={categories}
       />
     </header>
--- components/HeaderWithDropdown/parts/Dropdown.js
+++ components/HeaderWithDropdown/parts/Dropdown.js
@@ -2,42 +2,50 @@ import React from "react";
 import DropdownItem from "./DropdownItem";
 
 import { useState } from "react";
-const Dropdown = ({
-  categories,
-  isOpen,
-  toggleDropdown,
-  initialDropdownValue,
-}) => {
+import ClickAwayListener from "../../ClickAwayListener";
+const Dropdown = ({ categories, initialDropdownValue }) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const closeDropdown = () => {
+    setIsDropdownOpen(false);
+  };
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
-    <div className={"w-full lg:w-4/12"} data-name="dropdownMenu">
-      <div
-        className={`flex flex-col text-left w-full bg-white shadow-md dropdown ${
-          isOpen ? "dropdown--open" : ""
-        }`}
-      >
-        <button
-          className="h-16 px-6 border-2 border-zinc-400 hover:border-green text-left text-zinc-600 hover:text-green transition dropdown__button"
-          type="button"
-          id="dropdownMenuButton"
-          aria-expanded={isOpen}
-          onClick={toggleDropdown}
+    <div className={"w-full lg:w-4/12"}>
+      <ClickAwayListener onClickAway={closeDropdown}>
+        <div
+          className={`flex flex-col text-left w-full bg-white shadow-md dropdown ${
+            isDropdownOpen ? "dropdown--open" : ""
+          }`}
         >
-          {initialDropdownValue}
-        </button>
-        <ul
-          className="flex flex-col py-4 bg-white shadow-md z-10 dropdown__menu"
-          aria-labelledby="dropdownMenuButton"
-        >
-          {categories &&
-            categories.map((category) => (
-              <DropdownItem
-                key={category}
-                name={category}
-                active={category === initialDropdownValue ? true : false}
-              />
-            ))}
-        </ul>
-      </div>
+          <button
+            className="h-16 px-6 border-2 border-zinc-400 hover:border-green text-left text-zinc-600 hover:text-green transition dropdown__button"
+            type="button"
+            id="dropdownMenuButton"
+            aria-expanded={isDropdownOpen}
+            onClick={toggleDropdown}
+          >
+            {initialDropdownValue}
+          </button>
+          <ul
+            className="flex flex-col py-4 bg-white shadow-md z-10 dropdown__menu"
+            aria-labelledby="dropdownMenuButton"
+          >
+            {categories &&
+              categories.map((category) => (
+                <DropdownItem
+                  key={category}
+                  name={category}
+                  active={category === initialDropdownValue ? true : false}
+                />
+              ))}
+          </ul>
+        </div>
+      </ClickAwayListener>
     </div>
   );
 };
