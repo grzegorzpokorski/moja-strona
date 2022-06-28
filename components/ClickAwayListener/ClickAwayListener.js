964f13e87678bceba9fc80b1ad65b031266421c7
unknown <mr.pokorski@gmail.com>
2022-06-28 20:17:40 +0200

    Change: delegate detecting click away event to the useOnClickAway hook

--- components/ClickAwayListener/ClickAwayListener.js
+++ components/ClickAwayListener/ClickAwayListener.js
@@ -1,23 +1,9 @@
-import React, { useEffect, useRef } from "react";
+import { useRef } from "react";
+import useOnClickAway from "../../hooks/useOnClickAway";
 
 const ClickAwayListener = ({ children, onClickAway }) => {
   const containerRef = useRef(null);
-
-  useEffect(() => {
-    const handleAction = (event) => {
-      if (!containerRef.current || containerRef.current.contains(event.target))
-        return;
-      onClickAway();
-    };
-
-    document.addEventListener("mousedown", handleAction);
-    document.addEventListener("touchstart", handleAction);
-
-    return () => {
-      document.removeEventListener("mousedown", handleAction);
-      document.removeEventListener("touchstart", handleAction);
-    };
-  }, [containerRef, onClickAway]);
+  useOnClickAway(containerRef, () => onClickAway());
 
   return <div ref={containerRef}>{children}</div>;
 };
