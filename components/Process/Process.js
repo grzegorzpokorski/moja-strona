ed0f09d703877d6c0ba82c39c97902a00208e9f5
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-07-09 12:32:22 +0200

    Refactor Process component

--- components/Process/Process.js
+++ components/Process/Process.js
@@ -1,6 +1,6 @@
 import Header from "../Header";
 import Section from "../Section";
-import StepsList from "./parts/StepsList";
+import StepItem from "./parts/StepItem";
 
 const Process = ({ header, steps, bgColor }) => {
   return (
@@ -11,7 +11,12 @@ const Process = ({ header, steps, bgColor }) => {
         description={header.description}
         centered
       />
-      <StepsList steps={steps} />
+      <ul className="flex flex-col gap-6 md:gap-0 mt-16 lg:mt-24 process">
+        {steps &&
+          steps.map((step, i) => (
+            <StepItem key={`${i} ${step.title}`} {...step} />
+          ))}
+      </ul>
     </Section>
   );
 };
--- components/Process/parts/StepItem.js
+++ components/Process/parts/StepItem.js
@@ -1,7 +1,7 @@
 const StepItem = ({ title, content }) => {
   return (
     <li className="flex flex-row justify-end md:even:justify-start process__item">
-      <article className="w-10/12 md:w-5/12 flex flex-col gap-3 bg-white drop-shadow-md p-8">
+      <article className="w-10/12 md:w-5/12 flex flex-col gap-3 bg-white drop-shadow-md p-8 border-2 border-greenLight">
         <header className="flex flex-row gap-3 process__header">
           <span className="font-bold text-green text-lg process__counter"></span>
           <h3 className="font-bold text-lg">{title}</h3>
