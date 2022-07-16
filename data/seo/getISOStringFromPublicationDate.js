8f1cb8b2f386d426354162613957192af59b8373
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-07-17 00:25:10 +0200

    Fix bug in date parsing in getISOStringFromPublicationDate.js

--- data/seo/getISOStringFromPublicationDate.js
+++ data/seo/getISOStringFromPublicationDate.js
@@ -1,10 +1,10 @@
 const getISOStringFromPublicationDate = (date) => {
   const splittedDate = date.split(".");
-
+  console.log(splittedDate);
   return new Date(
-    Number(splittedDate[2]), // Year
-    Number(splittedDate[1]), // Month
-    Number(splittedDate[0]) // Day
+    Number(splittedDate[0]), // Year
+    Number(splittedDate[1] - 1), // Month
+    Number(splittedDate[2]) // Day
   ).toISOString();
 };
 
