59603c7c22bfc29be1080c7ec485a4da37d70881
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-07-16 16:37:35 +0200

    Add new component SharePostLinks
    SharePostsLinks omponent is a part of Post component

---
+++ components/Post/parts/SharePostLinks.js
@@ -0,0 +1,69 @@
+import Button from "../../Button";
+import siteUrl from "../../../data/seo/siteUrl";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faFacebook,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
+const SharePostLinks = ({ slug, title }) => {
+  const postUrl = `${siteUrl}/blog/${slug}`;
+
+  return (
+    <ul className={`flex flex-wrap gap-3`}>
+      <li>
+        <Button
+          href={`https://www.facebook.com/sharer/sharer.php?u=${postUrl}`}
+          label={
+            <>
+              <span className="sr-only">Udostepnij na facebooku.</span>
+              <FontAwesomeIcon icon={faFacebook} />
+            </>
+          }
+          size="social"
+        />
+      </li>
+      <li>
+        <Button
+          href={`https://twitter.com/intent/tweet?url=${postUrl}`}
+          label={
+            <>
+              <span className="sr-only">Udostepnij na twitterze.</span>
+              <FontAwesomeIcon icon={faTwitter} />
+            </>
+          }
+          size="social"
+        />
+      </li>
+      <li>
+        <Button
+          href={`https://www.linkedin.com/shareArticle?mini=true&url=${postUrl}`}
+          label={
+            <>
+              <span className="sr-only">Udostepnij na Linkedin.</span>
+              <FontAwesomeIcon icon={faLinkedin} />
+            </>
+          }
+          size="social"
+        />
+      </li>
+      <li>
+        <Button
+          href={`mailto:?subject=${title}&body=<a href="${postUrl}">${title}</a>`}
+          label={
+            <>
+              <span className="sr-only">Udostepnij emailem.</span>
+              <FontAwesomeIcon icon={faEnvelope} />
+            </>
+          }
+          size="social"
+        />
+      </li>
+    </ul>
+  );
+};
+
+export default SharePostLinks;
