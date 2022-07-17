b4a23b3f7340059036e92f14d5e1206ba7c3b771
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-07-17 17:44:40 +0200

    Set date in getISOStringFromPublicationDate to UTC time zone

--- data/seo/getISOStringFromPublicationDate.js
+++ data/seo/getISOStringFromPublicationDate.js
@@ -2,9 +2,11 @@ const getISOStringFromPublicationDate = (date) => {
   const splittedDate = date.split(".");
 
   return new Date(
-    Number(splittedDate[0]), // Year
-    Number(splittedDate[1] - 1), // Month
-    Number(splittedDate[2]) // Day
+    Date.UTC(
+      Number(splittedDate[0]), // Year
+      Number(splittedDate[1]) - 1, // Month
+      Number(splittedDate[2]) // Day
+    )
   ).toISOString();
 };
 
