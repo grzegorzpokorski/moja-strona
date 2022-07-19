0042b2f7847752718a26d7d27d943d26addd470f
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-07-19 15:03:03 +0200

    Change kontakt page structure

--- pages/kontakt.js
+++ pages/kontakt.js
@@ -1,14 +1,18 @@
+import Image from "next/image";
 import Head from "../components/Head";
-import MainHeader from "../components/MainHeader";
-import Hero from "../components/Hero";
+import Section from "../components/Section";
+import Header from "../components/Header";
 import Main from "../components/Main";
+import MainHeader from "../components/MainHeader";
 import Link from "../components/Link";
+import Banner from "../components/Banner";
 
 import addressSeparator from "../data/seo/addressSeparator";
 import siteName from "../data/seo/siteName";
-import heroImage from "../public/images/contact.webp";
 
-const Contact = () => {
+import image from "../public/images/contact.webp";
+
+const Pricing = () => {
   return (
     <>
       <Head
@@ -16,42 +20,62 @@ const Contact = () => {
         description={`Jeśli interesuje Cię współpraca lub po prostu chcesz pogadać, to tutaj znajdziesz kontakt do mnie!`}
         contentType="website"
       />
-      <MainHeader>
-        <Hero
-          title="Kontakt"
-          description={
-            <>
-              <p>
-                Jeśli interesuje Cię współpraca lub po prostu chcesz zadać
-                pytanie, nie bój się napisać na mojego maila:
-              </p>
-              <p>
-                <Link
-                  href="mailto:mr.pokorski@gmail.com"
-                  className="font-medium"
-                >
-                  mr.pokorski@gmail.com
-                </Link>
-              </p>
-              <p>
-                Zachęcam do zapozniana się z{" "}
-                <Link href="/czesto-zadawane-pytania">
-                  najczęściej zadawanymi pytaniami
-                </Link>
-                , gdzie być może znajdziesz odpowiedz na swoje pytanie.
-              </p>
-            </>
-          }
-          image={{
-            src: heroImage,
-            alt: "mężczyzna siedzący z laptopem na kolanach",
-          }}
-          withMarginOnTop
+      <MainHeader />
+      <Main withMarginOnTop>
+        <Section bgColor="bg-whiteGreen">
+          <Header
+            subtitle="Kontakt"
+            title="Porozmawiajmy o Twoich potrzebach"
+            description={
+              <>
+                <p>
+                  Jeśli interesuje Cię współpraca lub po prostu chcesz zadać
+                  pytanie, nie bój się napisać na mojego maila:{" "}
+                  <Link
+                    href="mailto:mr.pokorski@gmail.com"
+                    className="font-medium"
+                  >
+                    mr.pokorski@gmail.com
+                  </Link>{" "}
+                  lub skorzystać z formularza kontaktowego. Zachęcam do
+                  zapoznania się z{" "}
+                  <Link href="/czesto-zadawane-pytania">
+                    najczęściej zadawanymi pytaniami
+                  </Link>
+                  , gdzie być może znajdziesz odpowiedz na swoje pytanie.
+                </p>
+              </>
+            }
+            centered
+            titleAsH1
+            customClasses={`pb-16 md:pb-24`}
+          />
+          <div className="flex flex-col lg:flex-row gap-6">
+            <div className="w-full lg:w-1/2">{/* <ContactForm /> */}</div>
+            <picture className="w-full lg:w-1/2">
+              <Image src={image} alt={image.alt} />
+            </picture>
+          </div>
+        </Section>
+        <Banner
+          title="Zbuduj swoją ultra szybką stronę internetową ze mną!"
+          description="Chętnie pomogę zrealizować Twój projekt."
+          buttons={[
+            {
+              label: "Skontaktuj się ze mną",
+              href: "/kontakt",
+              style: "white",
+            },
+            {
+              label: "Zobacz ofertę",
+              href: "/oferta",
+              style: "transparentWhite",
+            },
+          ]}
         />
-      </MainHeader>
-      <Main></Main>
+      </Main>
     </>
   );
 };
 
-export default Contact;
+export default Pricing;
