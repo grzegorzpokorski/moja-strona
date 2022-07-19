61df2b6ef7a361f5d290db518fd7695393ed536d
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-07-20 00:54:34 +0200

    Add validation to form in  ContactForm component

--- components/ContactForm/ContactForm.js
+++ components/ContactForm/ContactForm.js
@@ -1,8 +1,23 @@
 import Link from "../Link";
+import { useForm } from "react-hook-form";
 
 const ContactForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <form className="bg-white shadow-md rounded-md px-8 py-10 border-2 border-greenLight">
+    <form
+      className="bg-white shadow-md rounded-md px-8 py-10 border-2 border-greenLight"
+      noValidate
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <div className="w-full mb-6">
         <label
           className="block text-gray-700 text-sm font-bold mb-2"
@@ -11,14 +26,25 @@ const ContactForm = () => {
           Imie *
         </label>
         <input
-          className="text-sm appearance-none border-2 border-gray-200 focus:border-green rounded w-full py-3 px-4 text-gray-600 focus:outline-none focus:shadow-outline bg-whiteGreen focus:bg-white transition duration-200"
+          className={`text-sm appearance-none border-2 ${
+            errors.name
+              ? "border-red-400"
+              : "border-gray-200 focus:border-green"
+          } rounded w-full py-3 px-4 text-gray-600 focus:outline-none focus:shadow-outline bg-whiteGreen focus:bg-white transition duration-200`}
           id="name"
           type="text"
           placeholder="Imie"
           aria-required="true"
-          aria-invalid="false"
-          required
+          aria-invalid={errors.name ? "true" : "false"}
+          {...register("name", { required: true, minLength: 2 })}
         />
+        {errors.name && (
+          <p className="text-sm text-red-400 mt-2">
+            {errors.name.type === "minLength"
+              ? "Pole powinno zawierać minimum 2 znaki."
+              : "Pole jest wymagane."}
+          </p>
+        )}
       </div>
       <div className="w-full mb-6">
         <label
@@ -28,14 +54,25 @@ const ContactForm = () => {
           Nazwisko *
         </label>
         <input
-          className="text-sm appearance-none border-2 border-gray-200 focus:border-green rounded w-full py-3 px-4 text-gray-600 focus:outline-none focus:shadow-outline bg-whiteGreen focus:bg-white transition duration-200"
+          className={`text-sm appearance-none border-2 ${
+            errors.surrname
+              ? "border-red-400"
+              : "border-gray-200 focus:border-green"
+          } rounded w-full py-3 px-4 text-gray-600 focus:outline-none focus:shadow-outline bg-whiteGreen focus:bg-white transition duration-200`}
           id="surrname"
           type="text"
           placeholder="Nazwisko"
           aria-required="true"
-          aria-invalid="false"
-          required
+          aria-invalid={errors.name ? "true" : "false"}
+          {...register("surrname", { required: true, minLength: 2 })}
         />
+        {errors.surrname && (
+          <p className="text-sm text-red-400 mt-2">
+            {errors.surrname.type === "minLength"
+              ? "Pole powinno zawierać minimum 2 znaki."
+              : "Pole jest wymagane."}
+          </p>
+        )}
       </div>
       <div className="w-full mb-6">
         <label
@@ -45,47 +82,83 @@ const ContactForm = () => {
           Adres email *
         </label>
         <input
-          className="text-sm appearance-none border-2 border-gray-200 focus:border-green rounded w-full py-3 px-4 text-gray-600 focus:outline-none focus:shadow-outline bg-whiteGreen focus:bg-white transition duration-200"
+          className={`text-sm appearance-none border-2 ${
+            errors.email
+              ? "border-red-400"
+              : "border-gray-200 focus:border-green"
+          } rounded w-full py-3 px-4 text-gray-600 focus:outline-none focus:shadow-outline bg-whiteGreen focus:bg-white transition duration-200`}
           id="email"
           type="email"
           placeholder="Adres email"
           aria-required="true"
-          aria-invalid="false"
-          required
+          aria-invalid={errors.email ? "true" : "false"}
+          {...register("email", {
+            required: true,
+            pattern: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
+          })}
         />
+        {errors.email && (
+          <p className="text-sm text-red-400 mt-2">
+            {errors.email.type === "pattern"
+              ? "Wprowadzony adres e-mail jest nieprawidłowy, sprawdź format (np. email@domena.com)."
+              : "Pole jest wymagane."}
+          </p>
+        )}
       </div>
       <div className="w-full mb-6">
         <label
           className="block text-gray-700 text-sm font-bold mb-2"
           htmlFor="message"
         >
-          Treść wiadomości / opis projektu
+          Treść wiadomości / opis projektu *
         </label>
         <textarea
-          className="text-sm appearance-none border-2 border-gray-200 focus:border-green rounded w-full h-36 lg:h-48 py-3 px-4 text-gray-600 focus:outline-none focus:shadow-outline bg-whiteGreen focus:bg-white transition duration-200"
+          className={`text-sm appearance-none border-2 ${
+            errors.message
+              ? "border-red-400"
+              : "border-gray-200 focus:border-green"
+          } rounded w-full py-3 px-4 text-gray-600 focus:outline-none focus:shadow-outline bg-whiteGreen focus:bg-white transition duration-200 h-36`}
           id="message"
           type="text"
           placeholder="Treść wiadomości / opis projektu"
-        />
-      </div>
-      <div className="flex items-center  mb-6">
-        <input
-          id="gdpr"
-          type="checkbox"
-          className="w-4 h-4 rounded border-green focus:ring-green focus:ring-2 accent-green"
           aria-required="true"
-          aria-invalid="false"
-          required
+          aria-invalid={errors.message ? "true" : "false"}
+          {...register("message", { required: true, minLength: 10 })}
         />
-        <label htmlFor="gdpr" className="ml-2 text-sm text-gray-500">
-          Wyrażam zgodę na przechowywanie przez tę witrynę przesłanych przeze
-          mnie informacji, w celu związanym z odpowiedzią na moje zapytanie. Aby
-          dowiedzieć się więcej, odwiedź{" "}
-          <Link href={"/"} className="text-green hover:underline">
-            politykę prywatności
-          </Link>
-          .
-        </label>
+        {errors.message && (
+          <p className="text-sm text-red-400 mt-2">
+            {errors.message.type === "minLength"
+              ? "Pole powinno zawierać minimum 10 znaków."
+              : "Pole jest wymagane."}
+          </p>
+        )}
+      </div>
+      <div className="flex flex-col mb-6">
+        <div className="flex">
+          <input
+            id="gdpr"
+            type="checkbox"
+            className="w-4 h-4 rounded border-green focus:ring-green focus:ring-2 accent-green cursor-pointer"
+            aria-required="true"
+            aria-invalid="false"
+            {...register("gdpr", { required: true })}
+          />
+          <label
+            htmlFor="gdpr"
+            className="ml-2 text-sm text-gray-500 cursor-pointer"
+          >
+            Wyrażam zgodę na przechowywanie przez tę witrynę przesłanych przeze
+            mnie informacji, w celu związanym z odpowiedzią na moje zapytanie.
+            Aby dowiedzieć się więcej, odwiedź{" "}
+            <Link href={"/"} className="text-green hover:underline">
+              politykę prywatności
+            </Link>
+            .
+          </label>
+        </div>
+        {errors.gdpr && (
+          <p className="text-sm text-red-400 mt-2">Zgoda jest wymagana.</p>
+        )}
       </div>
       <button
         className="inline-flex transition-colors border-2 rounded px-4 md:px-6 py-2 md:py-3 text-base bg-green hover:bg-greenHover text-white border-green"
