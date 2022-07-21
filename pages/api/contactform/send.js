324adf99f8fda5112baac0ec124e23df79e6b76b
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-07-21 02:37:52 +0200

    Sanitize message content in api/contactform/send

--- pages/api/contactform/send.js
+++ pages/api/contactform/send.js
@@ -1,4 +1,5 @@
 import sendgrid from "@sendgrid/mail";
+import sanitizeHtml from "sanitize-html";
 import { host } from "../../../data/seo/siteUrl";
 
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
@@ -9,7 +10,13 @@ const send = async (req, res) => {
       to: process.env.SENDGRID_MESSAGE_RECIEVER,
       from: process.env.SENDGRID_MESSAGE_RECIEVER,
       subject: `Wiadomość ze strony ${host}`,
-      html: `<p>imie: ${req.body.name}<br>nazwisko: ${req.body.surrname}<br>email: ${req.body.email}</p><p>${req.body.message}</p>`,
+      html: `<p>imie: ${sanitizeHtml(req.body.name, {
+        allowedTags: [],
+      })}<br>nazwisko: ${sanitizeHtml(req.body.surrname, {
+        allowedTags: [],
+      })}<br>email: ${req.body.email}</p><p>${sanitizeHtml(req.body.message, {
+        allowedTags: [],
+      })}</p>`,
     });
   } catch (error) {
     return res.status(error.statusCode || 500).json({ error: error.message });
