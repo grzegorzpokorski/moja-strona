e61c70a5a1ce17ba3169f01d5fdfb5ccc9915806
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-08-06 01:07:27 +0200

    change id field requirement to optional in Accordion component

--- components/Accordion/Accordion.tsx
+++ components/Accordion/Accordion.tsx
@@ -1,18 +1,27 @@
-import AccordionItem from './parts/AccordionItem';
-import { FC, useState } from 'react';
-import { FAQItem } from './../../types/content';
+import AccordionItem from "./parts/AccordionItem";
+import { FC, useState } from "react";
+import { FAQItem } from "./../../types/content";
 
 type AccordionType = {
   items: FAQItem[];
   withMarginOnTop?: boolean;
-  id: string;
+  id?: string;
 };
 
-const Accordion: FC<AccordionType> = ({ items, withMarginOnTop, id = 'accordion' }) => {
+const Accordion: FC<AccordionType> = ({
+  items,
+  withMarginOnTop,
+  id = "accordion",
+}) => {
   const [expandedItem, setExpandedItem] = useState<string>(items[0].question);
 
   return (
-    <div className={`flex flex-col gap-4 mx-auto ${withMarginOnTop ? 'mt-12 md:mt-24' : ''} accordion`} id={id}>
+    <div
+      className={`flex flex-col gap-4 mx-auto ${
+        withMarginOnTop ? "mt-12 md:mt-24" : ""
+      } accordion`}
+      id={id}
+    >
       {items.map((item, i) => (
         <AccordionItem
           key={item.question}
@@ -23,7 +32,7 @@ const Accordion: FC<AccordionType> = ({ items, withMarginOnTop, id = 'accordion'
             if (expandedItem != item.question) {
               setExpandedItem(item.question);
             } else {
-              setExpandedItem('');
+              setExpandedItem("");
             }
           }}
         />
