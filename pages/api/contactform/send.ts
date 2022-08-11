a27ebff60cd0954b28bf451c26e7819a816d464d
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-08-11 23:21:08 +0200

    rewrite /pages/api/contactform/send to ts

--- pages/api/contactform/send.ts
+++ pages/api/contactform/send.ts
@@ -1,14 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from "next";
 import sendgrid from "@sendgrid/mail";
 import sanitizeHtml from "sanitize-html";
 import { host } from "../../../data/seo/siteUrl";
 
-sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY as string);
 
-const send = async (req, res) => {
+type Data = {
+  error: string;
+};
+
+const send = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  console.log(req);
   try {
     await sendgrid.send({
-      to: process.env.SENDGRID_MESSAGE_RECIEVER,
-      from: process.env.SENDGRID_MESSAGE_RECIEVER,
+      to: process.env.SENDGRID_MESSAGE_RECIEVER as string,
+      from: process.env.SENDGRID_MESSAGE_RECIEVER as string,
       replyTo: sanitizeHtml(req.body.email),
       subject: `Wiadomość ze strony ${host}`,
       html: `<p>imie: ${sanitizeHtml(req.body.name, {
@@ -19,11 +25,12 @@ const send = async (req, res) => {
         req.body.message,
         {
           allowedTags: [],
-        }
+        },
       )}</p>`,
     });
-  } catch (error) {
-    return res.status(error.statusCode || 500).json({ error: error.message });
+  } catch (e) {
+    const error = e as Error;
+    return res.status(500).json({ error: error.message });
   }
 
   return res.status(200).json({ error: "" });
