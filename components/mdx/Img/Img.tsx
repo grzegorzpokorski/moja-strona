ec8051764b1de670dbed6ca52bccef5371433f45
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-08-13 17:33:16 +0200

    add a width attribute to  /components/mdx/Img image

--- components/mdx/Img/Img.tsx
+++ components/mdx/Img/Img.tsx
@@ -5,7 +5,9 @@ type MdxImageProps = {
 };
 
 const MdxImage = ({ alt, src }: MdxImageProps) => {
-  return <img src={src} alt={alt} loading="lazy" decoding="async" />;
+  return (
+    <img src={src} alt={alt} width="100%" loading="lazy" decoding="async" />
+  );
 };
 
 export default MdxImage;
