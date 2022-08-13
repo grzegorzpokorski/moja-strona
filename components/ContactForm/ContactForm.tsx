fe11ad783111fc1418195e3b508df46eed5494bf
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-08-13 16:16:45 +0200

    rename a state variable in ContactForm component

--- components/ContactForm/ContactForm.tsx
+++ components/ContactForm/ContactForm.tsx
@@ -58,18 +58,18 @@ const ContactForm = () => {
     resolver: yupResolver(validationSchema),
   });
 
-  const [sendingState, setSendingState] = useState<
+  const [sendingStatus, setSendingStatus] = useState<
     "initial" | "sending" | "sended" | "error"
   >("initial");
 
   const onSubmit = async (data: ContactFormValues) => {
-    setSendingState("sending");
+    setSendingStatus("sending");
     const sendingError = await sendMessageFromContactForm(data);
 
     if (sendingError) {
-      setSendingState("error");
+      setSendingStatus("error");
     } else {
-      setSendingState("sended");
+      setSendingStatus("sended");
       reset();
     }
   };
@@ -82,7 +82,7 @@ const ContactForm = () => {
     >
       <fieldset
         className={`disabled:cursor-not-allowed`}
-        disabled={sendingState && sendingState !== "initial"}
+        disabled={sendingStatus && sendingStatus !== "initial"}
       >
         <legend className={`sr-only`}>Formularz kontaktowy</legend>
         <div className={`w-full mb-6`}>
@@ -202,30 +202,30 @@ const ContactForm = () => {
         </div>
         <button
           className={`inline-flex items-center gap-2 transition-colors border-2 rounded px-4 md:px-6 py-2 md:py-3 text-base text-white disabled:cursor-not-allowed ${
-            sendingState && sendingState === "error"
+            sendingStatus && sendingStatus === "error"
               ? "bg-red-400 border-red-400"
               : "bg-green hover:bg-greenHover border-green"
           }`}
         >
-          {sendingState && sendingState === "initial" && (
+          {sendingStatus && sendingStatus === "initial" && (
             <>
               Wyślij wiadomość
               <FontAwesomeIcon icon={faPaperPlane} />
             </>
           )}
-          {sendingState && sendingState === "sending" && (
+          {sendingStatus && sendingStatus === "sending" && (
             <>
               Wysyłanie
               <FontAwesomeIcon icon={faCircleNotch} className="animate-spin" />
             </>
           )}
-          {sendingState && sendingState === "sended" && (
+          {sendingStatus && sendingStatus === "sended" && (
             <>
               Wysłano
               <FontAwesomeIcon icon={faCircleCheck} className="animate-scale" />
             </>
           )}
-          {sendingState && sendingState === "error" && (
+          {sendingStatus && sendingStatus === "error" && (
             <>
               Wystąpił błąd. Spróbuj ponownie pózniej
               <FontAwesomeIcon icon={faCircleExclamation} />
