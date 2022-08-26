1c551f8f5db53431e15cc2cada8c9d1159bed4e1
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-08-26 17:01:24 +0200

    rewrite 'utils/posts.ts' to use async functions

--- utils/posts.ts
+++ utils/posts.ts
@@ -1,16 +1,11 @@
-import fs from "fs";
+import { promises as fs } from "fs";
 import path from "path";
 import matter from "gray-matter";
+import { MDXRemoteSerializeResult } from "next-mdx-remote";
 
 import dayjs from "dayjs";
 import customParseFormat from "dayjs/plugin/customParseFormat";
 dayjs.extend(customParseFormat);
-
-import { MDXRemoteSerializeResult } from "next-mdx-remote";
-
-const rootPath = process.cwd();
-const postsPath = path.join(rootPath, "data", "posts");
-const mainFileName = "index.mdx";
 
 export type PostFrontmatter = {
   title: string;
@@ -19,8 +14,8 @@ export type PostFrontmatter = {
   tags: string[];
   date: string;
   published: boolean;
-  slug: string;
   featuredImage: string;
+  slug: string;
 };
 
 export type PostWithRawSource = {
@@ -33,27 +28,11 @@ export type PostWithCompiledSource = {
   source: MDXRemoteSerializeResult<Record<string, unknown>>;
 };
 
-export const getSlugs = () => {
-  const slugs = fs
-    .readdirSync(postsPath, { withFileTypes: true })
-    .filter((dirent) => dirent.isDirectory())
-    .map((dirent) => dirent.name);
+const postsDir = path.join(process.cwd(), "data/posts");
 
-  return slugs;
-};
-
-export const getPostsPaths = () => {
-  const slugs = getSlugs();
-  const paths = slugs.map((slug) => ({ params: { slug } }));
-
-  return paths;
-};
-
-export const getPostBySlug = (
-  slug: string,
-): { frontmatter: PostFrontmatter; source: string } => {
-  const mainFilePath = path.join(postsPath, slug, mainFileName);
-  const postSource = fs.readFileSync(mainFilePath).toString();
+export const getPostBySlug = async (slug: string): Promise<PostWithRawSource> => {
+  const postPath = path.join(postsDir, `${slug}.mdx`);
+  const postSource = await fs.readFile(postPath, { encoding: "utf-8" });
   const { content, data } = matter(postSource);
 
   return {
@@ -64,24 +43,22 @@ export const getPostBySlug = (
       tags: data.tags,
       date: data.date,
       published: data.published,
-      slug: data.slug,
       featuredImage: data.featuredImage,
+      slug,
     },
     source: content,
   };
 };
 
-export const getAllPosts = () => {
-  const slugs = getSlugs();
-  const posts = slugs.map((slug) => getPostBySlug(slug));
+export const getAllPosts = async (): Promise<PostWithRawSource[]> => {
+  const slugs = await getAllSlugs();
+  const posts = slugs.map(async (slug) => await getPostBySlug(slug));
 
-  return posts;
+  return Promise.all(posts);
 };
 
-export const getPublishedPostsOrderByDate = () => {
-  const posts = getAllPosts();
-  const published = posts.filter((post) => post.frontmatter.published == true);
-  const sorted = published.sort((a, b) => {
+export const sortPostsByPublishedDate = (posts: PostWithRawSource[] | PostWithCompiledSource[]) => {
+  const sorted = posts.sort((a, b) => {
     const first = dayjs(a.frontmatter.date, "YYYY.MM.DD");
     const second = dayjs(b.frontmatter.date, "YYYY.MM.DD");
 
@@ -91,18 +68,35 @@ export const getPublishedPostsOrderByDate = () => {
   return sorted;
 };
 
-export const getTags = () => {
-  const posts = getPublishedPostsOrderByDate();
-  const tags = posts.map((post) => post.frontmatter.tags).flat();
-  const uniqueTags = new Set(tags);
+export const getPublishedPosts = async () => {
+  const posts = await getAllPosts();
 
-  return Array.from(uniqueTags);
+  return posts.filter((post) => post.frontmatter.published === true);
 };
 
-export const getCategories = () => {
-  const posts = getPublishedPostsOrderByDate();
-  const categories = posts.map((post) => post.frontmatter.category);
-  const uniqueCategories = new Set(categories);
+export const getAllSlugs = async () => {
+  const allFileNames = await fs.readdir(postsDir);
+  const onlyMdx = allFileNames.filter((file) => path.extname(file.toLowerCase()) === ".mdx");
 
-  return Array.from(uniqueCategories);
+  return onlyMdx.map((file) => path.basename(file, ".mdx"));
+};
+
+export const getSlugsOfPublishedPosts = async () => {
+  const publishedPosts = await getPublishedPosts();
+
+  return publishedPosts.map((post) => post.frontmatter.slug);
+};
+
+export const getCategories = async () => {
+  const posts = await getPublishedPosts();
+  const categories = new Set(posts.map((post) => post.frontmatter.category));
+
+  return Array.from(categories);
+};
+
+export const getTags = async () => {
+  const posts = await getPublishedPosts();
+  const tags = new Set(posts.map((post) => post.frontmatter.tags).flat());
+
+  return Array.from(tags);
 };
