1ab8986525f4e778f9cbf08ed6dfdf27b7def4a3
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-08-27 16:19:03 +0200

    restore old version of next-sitemap config file

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,11 @@
-const origin = process.env.NEXT_PUBLIC_HOST || process.env.NEXT_PUBLIC_VERCEL_URL || "grzegorzpokorski.pl";
+const productionHost = process.env.NEXT_PUBLIC_HOST || "grzegorzpokorski.pl";
+const devHost = "localhost:3000";
+const host = process.env.NODE_ENV === "production" ? productionHost : devHost;
+
 const protocol = process.env.NODE_ENV === "production" ? "https" : "http";
-const host = `${protocol}://${origin}`;
 
 module.exports = {
-  siteUrl: host,
+  siteUrl: `${protocol}://${host}`,
   generateRobotsTxt: true,
   exclude: ["/admin"],
 };
