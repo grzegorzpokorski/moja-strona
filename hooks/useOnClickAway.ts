d14542a7d75251e670084723995673dcab9ef805
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-08-31 01:26:56 +0200

    refactor useOnClickAway hook

--- hooks/useOnClickAway.ts
+++ hooks/useOnClickAway.ts
@@ -1,14 +1,12 @@
 import { RefObject, useEffect } from "react";
 
-const useOnClickAway = (
-  ref: RefObject<HTMLElement>,
-  action: () => void,
-): void => {
+const useOnClickAway = (ref: RefObject<Node>, action: (event: MouseEvent | TouchEvent) => void): void => {
   useEffect(() => {
-    const handleAction = (event: MouseEvent) => {
-      if (!ref.current || ref.current.contains(event.target as HTMLElement))
-        return;
-      action();
+    const handleAction = (event: MouseEvent | TouchEvent) => {
+      if (event.target instanceof Node) {
+        if (!ref.current || ref.current.contains(event.target)) return;
+        action(event);
+      }
     };
 
     document.addEventListener("mousedown", handleAction);
